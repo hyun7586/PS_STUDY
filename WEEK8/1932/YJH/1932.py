87bed23bb19d15914963632057782ef27e93f589
letsgojh <dkflaowghks123456@gmail.com>
2024-05-15 11:33:41 +0000

    YJH create 1932

---
+++ WEEK8/1932/YJH/1932.py
@@ -0,0 +1,35 @@
+import sys
+
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+
+    n = int(input())
+    triangle = []
+    dp = [[0 for k in range(i+1)] for i in range(n)]
+
+
+    for i in range(n):    
+        triangle.append(list(map(int,input().split())))
+
+    #print(*triangle, sep = '\n');
+
+    dp[0][0] = triangle[0][0]
+
+    #print(*dp, sep = '\n')
+
+    for i in range(1,n):
+        for k in range(i+1):
+
+            if k == 0:
+                dp[i][k] = triangle[i][k] + dp[i-1][k]
+
+            elif k == i:
+                dp[i][k] = triangle[i][k] + dp[i-1][k-1]
+
+            else:
+                dp[i][k] = max(triangle[i][k] + dp[i-1][k-1], triangle[i][k] + dp[i-1][k])
+
+    ans = max(dp[n-1])
+
+    print(ans)
